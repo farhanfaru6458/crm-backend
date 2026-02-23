b5ffe6a3974ebcc0808f49102883c8626ddd05d6
Farhan Faru <farhanfaru6458@gmail.com>
2026-02-23 11:11:36 +0530

    added few changes in the folder structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import errorHandler from "./middleware/errorMiddleware";
+import errorHandler from "./middleware/errorMiddleware.js";
 
 const app = express();
 
