9b8a0f853f2241b67b5165b979d1908d72910848
Farhan Faru <farhanfaru6458@gmail.com>
2026-02-23 10:52:53 +0530

    added Folder Structure

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import errorHandler from "./middleware/errorMiddleware";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(errorHandler);
+app.get("/", (req, res) => {
+  res.send("CRM Backend Running");
+});
+
+export default app;
