da1ea7a0dd5bb05822dca9ef9fb91efac1ba9778
dhanush8714 <dhanushmp52@gmail.com>
2026-02-26 11:57:34 +0530

    Implemented auth backend

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import errorHandler from "./middleware/errorMiddleware.js";
+import authRoutes from "./routes/authRoutes.js";
+
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("CRM Backend Running");
 });
---
+++ src/models/User.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    industry: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
